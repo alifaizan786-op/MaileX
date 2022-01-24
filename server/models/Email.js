a8b3a6d8580169bd7b061968dfab72685e82c2ee
alifaizan786-op <faizan.ali@irunadigital.com>
2022-01-24 17:14:11 -0500

    added email & index model

--- server/models/Email.js
+++ server/models/Email.js
@@ -1,1 +1,31 @@
 const { Schema, model } = require('mongoose');
+
+
+const emailSchema = new Schema({
+    sender : {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    recipient : {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    emailbody : {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    sentDate : {
+        type : Date,
+        default: Date.now
+    },
+})
+
+const Email = model('Email', emailSchema);
+
+module.exports = Email;
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const Email = require('./Email')
+const User = require('./User')
+
+module.exports = { Email, User }
