fa5def0f15d357c9fbc67b53eefa8e6e94a518f6
alifaizan786-op <faizan.ali@irunadigital.com>
2022-01-24 01:33:09 -0500

    added user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,32 @@
 const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema({
+    firstName : {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    lastName : {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
