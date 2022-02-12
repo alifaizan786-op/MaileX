e7c5fda4e45dee23b3116c61858cc0ed9539e661
alifaizan786-op <faizan.ali@irunadigital.com>
2022-02-12 12:00:39 -0500

    pushing to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -68,7 +68,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Router>
+      <Router basename='/MaileX'>
         <Route exact path="/">
           <ApolloProvider client={client}>
             {Auth.loggedIn() ? (
